33800690b4d7ca37668e964a954f6eaf5afa30c1
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2022-11-30 06:01:43 +0000

    style: styled fetched data

--- lists/pages/ninjas/index.js
+++ lists/pages/ninjas/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import styles from "../../styles/Ninjas.module.css"
 import Link from "next/link";
 
 export const getStaticProps = async () =>{
@@ -16,15 +17,13 @@ const Ninjas = ({ ninjas }) => {
         <title>next | List</title>
         <meta name="keywords" content="next" />
       </Head>
-        <div>
-            <h1>all Ninjas</h1>
+        <div className={styles.ninjas__container}>
+            <h1>All Ninjas</h1>
             {ninjas.map(ninja =>(
                 <div key={ ninja.id }>
-                   
-                   <h3>
+                   <h3 className={styles.ninja}>
                     <a>{ ninja.name }</a>
                     </h3>
-                   
                 </div>
             ))
 
